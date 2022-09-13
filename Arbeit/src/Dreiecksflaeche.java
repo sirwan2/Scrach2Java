95017792ebb91b8010cf057cf46e0c53b2603059
sirwan2 <113333929+sirwan2@users.noreply.github.com>
2022-09-13 13:21:26 +0200

    Update .gitignore

---
+++ Arbeit/src/Dreiecksflaeche.java
@@ -0,0 +1,10 @@
+public class Dreiecksflaeche {
+      public static void main (String[]args) {
+
+          double h = 6;
+          double h = 8;
+          double A = 0.5 * H * 6;
+          System.out.println();
+      }
+
+}
---
+++ Scrch2Java.1/src/ZweitesProgramm.java
@@ -0,0 +1,12 @@
+public class ZweitesProgramm {
+
+    public static void main (String[] args)  {
+        int zahl1;
+        int zahl2;
+        int ergebnis;
+        zahl1=5;
+        zahl2=4;
+        ergebnis= zahl1+zahl2 ;
+        System.out.println(ergebnis);
+    }
+}
