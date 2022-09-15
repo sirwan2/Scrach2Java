760923159c738553ea08c6280a61af8e0e15e988
sirwan2 <sirwan2@schule.bremen.de>
2022-09-15 15:55:57 +0200

    Update .gitignore

--- Arbeit/src/Wetter.java
+++ Arbeit/src/Wetter.java
@@ -3,12 +3,12 @@ public class Wetter {
         double april = 14.6;
         double mai = 8.4;
         double juni = 9.3;
-        double Mittelwert = april + mai + juni;
-        double ergebnis = Mittelwert / 3;
+        double mittelwert = april + mai + juni;
+        double ergebnis = mittelwert / 3;
 
-        System.out.println("Regen im april :"+april);
+        System.out.println("Regen im April :"+april);
         System.out.println("Regen im mai"+mai);
-        System.out.println("Regen im juni"+juni);
+        System.out.println("Regen im Juni"+juni);
         System.out.println("Mittelwert "+ergebnis);
     }
 
