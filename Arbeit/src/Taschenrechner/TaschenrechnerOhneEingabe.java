a1f187d251ab0a57627587b93447114a55c2757d
sirwan2 <sirwan2@schule.bremen.de>
2022-09-29 15:57:42 +0200

    Update .gitignore

---
+++ Arbeit/src/Taschenrechner/TaschenrechnerOhneEingabe.java
@@ -0,0 +1,17 @@
+package Taschenrechner;
+
+public class TaschenrechnerOhneEingabe {
+    public static void main(String[] args) {
+     double Zahl;
+     Zahl=2;
+     double Zahl2;
+     Zahl2=5;
+     double ergebnis;
+
+     ergebnis=(Zahl+Zahl2);
+     System.out.println(("Die summe beträgt"+ergebnis));
+
+     }
+
+    }
+
