4e5dda8592ba0be0785380a6861f11d7cd873c34
sirwan2 <sirwan2@schule.bremen.de>
2022-09-29 14:35:20 +0200

    Update .gitignore

---
+++ Arbeit/src/schleife.java
@@ -0,0 +1,10 @@
+public class schleife {
+    public static void main(String[]args){
+        // Ausgeben die ersten 10  Zahlen
+    int summe = 0;
+    for (int i=0; i <= 3; i = i +1){
+        summe = summe * i;
+    }
+      System.out.println(summe);
+}
+}
