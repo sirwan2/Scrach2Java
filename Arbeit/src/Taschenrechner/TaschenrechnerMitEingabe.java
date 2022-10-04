1fac57f1e2c2a569f29c3441e578194acf040e44
sirwan2 <sirwan2@schule.bremen.de>
2022-10-04 11:28:17 +0200

    Update .gitignore

---
+++ Arbeit/src/Taschenrechner/TaschenrechnerMitEingabe.java
@@ -0,0 +1,11 @@
+package Taschenrechner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TaschenrechnerMitEingabe {
+    public static void main(String[] args) {
+
+    }
+}
