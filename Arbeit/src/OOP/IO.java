3da967dd6b19b1d471d532a131e33e1608d296a4
sirwan2 <sirwan2@schule.bremen.de>
2022-11-17 15:39:19 +0100

    Update .gitignore

--- Arbeit/src/OOP/IO.java
+++ Arbeit/src/OOP/IO.java
@@ -5,23 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class IO {
+    /**
+     * liest einen Wahrheitswert von der Konsole ein
+     *
+     * @return Der Wahrheitswert
+     * @throws IOException
+     */
+
     public static String readString() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String eingabe = br.readLine();
         return eingabe;
     }
-    public static int readInteger() throws IOException{
-        while (true){
+
+    public static int readInteger() throws IOException {
+        while (true) {
             String text = readString();
             try {
                 int zahl = Integer.parseInt(text);
                 return zahl;
-            }
-            catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 System.out.println("Ungültige Eingabe. Bitte erneut versuchen");
             }
         }
     }
 
+    public static boolean readDouble() throws IOException {
+        while (true) {
+            String eingabe = readString();
+            boolean nein = eingabe.equalsIgnoreCase("nein");
+            if (eingabe.equals("J") || (eingabe.equalsIgnoreCase("ja"))) {
+                return true;
+            } else if ((eingabe.equals("nein")) || (eingabe.equalsIgnoreCase("nein")))
+                return false;
+        }
 
+
+    }
+
+
+    public static boolean readboolen() {
+
+        return false;
+    }
 }
+
--- Arbeit/src/OOP/Testio.java
+++ Arbeit/src/OOP/Testio.java
@@ -7,6 +7,18 @@ public class Testio {
         System.out.println("gib was ein:");
         String was = IO.readString();
         System.out.println(was);
+
+        System.out.println("gib ganzzahl du Kerr");
+        int zahl1 =IO.readInteger();
+        System.out.println(zahl1);
+
+        System.out.println("gib kommazahl");
+        int zahl2 =IO.readInteger();
+        System.out.println(zahl2);
+
+        System.out.println("weiterlaufen mit programm?");
+
+        while (IO.readboolen());
     }
 
 }
